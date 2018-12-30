2b71727c232bcfd085e64b4667bbbcd089942a95
Julian Napolitan <julian.napolitan@gmail.com>
2018-12-29 17:40:45 -0800

    Add user credential validation

---
+++ models/Stat.js
@@ -0,0 +1,19 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const StatSchema = new Schema({
+    user: {
+        type: Schema.Types.ObjectId,
+        required: true
+    },
+    score: {
+        type: Number,
+        required: true
+    },
+    date: {
+        type: Date,
+        default: Date.now
+    }
+});
+
+module.exports = Stat = mongoose.model('stats', StatSchema);
---
+++ routes/api/stats.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const router = express.Router();
+const Stat = require('../../models/Stat');
+
+router.post('/',
+    passport.authenticate('jwt', { session: false }), (req, res) => {
+  
+      const newStat = new Stat({
+        user: req.user.id,
+        score: req.body.score
+      });
+  
+      newStat.save().then(stat => res.json(stat));
+    }
+  );
+
+module.exports = router;
