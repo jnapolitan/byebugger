dd909d20e3773b77458a65353bd693f64927df81
Eric To <erto@berkeley.edu>
2019-01-05 22:38:18 -0800

    Localize text image

--- frontend/src/components/Main.jsx
+++ frontend/src/components/Main.jsx
@@ -20,13 +20,13 @@ export default class Main extends React.Component {
   render() {
     // TODO: Intro music - this is a semi-functional placeholder
     // By Eric Matyas @ https://soundimage.org/
-    let musicURL = "http://soundimage.org/wp-content/uploads/2016/07/Puzzle-Game_Looping.mp3";
-    let audio = new Audio(musicURL);
-    audio.play();
+    // let musicURL = "http://soundimage.org/wp-content/uploads/2016/07/Puzzle-Game_Looping.mp3";
+    // let audio = new Audio(musicURL);
+    // audio.play();
 
     return (
       <div id="splash" className="splash-container">
-        <img className="logo" src="https://static1.textcraft.net/data1/4/7/47ec57212c1063d986640e55e8fffed17cc1603fda39a3ee5e6b4b0d3255bfef95601890afd80709da39a3ee5e6b4b0d3255bfef95601890afd8070911e0e0a6c9273f3b1ad5cf500cddc6e2.png" alt="ByeBugger" />
+        <img className="logo" src="../assets/images/splashText.png" alt="ByeBugger" />
         <button className="start-button" onClick={ this.initiateGame }>START</button>
       </div>
     );
