3e6fcef4fca3863c34d9a9ba037ba5b23538631c
Eric To <erto@berkeley.edu>
2019-01-05 21:02:32 -0800

    Remove unused files

--- frontend/src/components/Main.jsx
+++ frontend/src/components/Main.jsx
@@ -1,13 +1,17 @@
-import React from 'react';
-import Game from '../game/game';
+import React from "react";
+
+import Game from "../game/game";
 
 export default class Main extends React.Component {
-
+  // e is pulled in as the "event" being handled
   initiateGame(e) {
+    // Don't reload the page upon clicking the start game button
     e.preventDefault();
-    const splash = document.getElementById('splash');
-    // splash.classList.add('hidden');
+    const splash = document.getElementById("splash");
+    // Hide the splash screen
+    splash.classList.add("hidden");
 
+    // Start the game
     const game = new Game();
     game.init();
     game.animate();
@@ -15,10 +19,10 @@ export default class Main extends React.Component {
 
   render() {
     return (
-      <div className="start-screen-container" id="splash">
-        <img className="logo" src="https://static1.textcraft.net/data1/4/7/47ec57212c1063d986640e55e8fffed17cc1603fda39a3ee5e6b4b0d3255bfef95601890afd80709da39a3ee5e6b4b0d3255bfef95601890afd8070911e0e0a6c9273f3b1ad5cf500cddc6e2.png" />
-        <button onClick={this.initiateGame} className="start-button">START</button>
+      <div id="splash" className="splash-container">
+        <img className="logo" src="https://static1.textcraft.net/data1/4/7/47ec57212c1063d986640e55e8fffed17cc1603fda39a3ee5e6b4b0d3255bfef95601890afd80709da39a3ee5e6b4b0d3255bfef95601890afd8070911e0e0a6c9273f3b1ad5cf500cddc6e2.png" alt="ByeBugger" />
+        <button className="start-button" onClick={ this.initiateGame }>START</button>
       </div>
-    )
+    );
   }
 }
