4966f7cde50364d651f35865563490523aadf3dc
Julian Napolitan <julian.napolitan@gmail.com>
2019-01-06 17:14:35 -0800

    Clean up test code

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -2,7 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import Root from './components/root';
 import configureStore from './store/store';
-import { fetchStats, receiveNewStat } from './actions/stat_actions';
 
 document.addEventListener('DOMContentLoaded', () => {
   const store = configureStore();
