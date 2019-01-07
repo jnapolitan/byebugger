b1116be387f11acbca4ff7be0d2cdfac420181be
Julian Napolitan <julian.napolitan@gmail.com>
2019-01-06 17:51:23 -0800

    Fix Hud jsx component

--- frontend/src/components/hud/hud.jsx
+++ frontend/src/components/hud/hud.jsx
@@ -2,8 +2,8 @@ import React from 'react';
 
 const Hud = props => (
   <div className="hud-container">
-    <h2>Health: {this.props.health}</h2>
-    <h2>Score: {this.props.score}</h2>
+    <h2>Health: {props.health}</h2>
+    <h2>Score: {props.score}</h2>
   </div>
 )
 
