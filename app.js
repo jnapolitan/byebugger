c82fb5f4c7aa02c54a4e87145fd12bf022fa7558
Julian Napolitan <julian.napolitan@gmail.com>
2019-01-08 16:09:48 -0800

    Finish heroku config

--- app.js
+++ app.js
@@ -6,11 +6,19 @@ const stats = require('./routes/api/stats');
 const bodyParser = require('body-parser');
 const passport = require('passport');
 const port = process.env.PORT || 5000;
+const path = require('path');
 
 mongoose
   .connect(db, { useNewUrlParser: true })
   .then(() => console.log("Connected to MongoDB successfully"))
   .catch(err => console.log(err));
+
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static('frontend/build'));
+  app.get('/', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
+}
 
 app.listen(port, () => console.log(`Server is running on port ${port}`));
 
